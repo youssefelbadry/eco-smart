5470f482ba20502ea442e982e5cffab1509887d6
youssefelbadry <youssefelbadry10@gmail.com>
2026-06-24 02:45:20 +0300

    v2

--- public/index.php
+++ public/index.php
@@ -18,6 +18,19 @@ $routes = [
         '/api_alert_acknowledge_all.php' => function () use ($config) { require __DIR__ . '/../src/Controllers/AlertController.php'; },
     ],
     'GET' => [
+        '/probe' => function () {
+            $log = [
+                'time' => date('c'),
+                'uri' => $_SERVER['REQUEST_URI'] ?? null,
+                'method' => $_SERVER['REQUEST_METHOD'] ?? null,
+                'remote_addr' => $_SERVER['REMOTE_ADDR'] ?? null,
+                'user_agent' => $_SERVER['HTTP_USER_AGENT'] ?? null,
+                'auth' => $_SERVER['HTTP_AUTHORIZATION'] ?? ($_SERVER['REDIRECT_HTTP_AUTHORIZATION'] ?? null),
+                'input' => file_get_contents('php://input'),
+            ];
+            @file_put_contents(__DIR__ . '/../logs/requests.log', json_encode($log, JSON_UNESCAPED_UNICODE) . PHP_EOL, FILE_APPEND);
+            \App\Helpers\ResponseHelper::success(['probe' => true, 'logged' => true], 'OK', 200);
+        },
         '/api_devices_list.php' => function () { require __DIR__ . '/../src/Controllers/DeviceController.php'; },
         '/api_devices_dashboard.php' => function () { require __DIR__ . '/../src/Controllers/DeviceController.php'; },
         '/api_categories_counts.php' => function () { require __DIR__ . '/../src/Controllers/DeviceController.php'; },
